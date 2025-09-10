43dcec8bce262e9b5381d2e88defc169d510cf8b
lsafer-meemer <135058778+lsafer-meemer@users.noreply.github.com>
2025-09-10 04:21:57 +0300

    fix: platform declaration clash

--- compose-iframe/src/commonMain/kotlin/Extensions.kt
+++ compose-iframe/src/commonMain/kotlin/Extensions.kt
@@ -5,19 +5,24 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.decodeFromJsonElement
 import kotlinx.serialization.json.encodeToJsonElement
+import kotlin.jvm.JvmName
 
+@JvmName("emit")
 suspend fun IframeState.emit(data: JsonElement, targetOrigin: String = "*") {
     outgoing.send(IframeOutgoingEvent(data, targetOrigin))
 }
 
+@JvmName("emitT")
 suspend inline fun <reified T> IframeState.emit(data: T, targetOrigin: String = "*") {
     outgoing.send(IframeOutgoingEvent(Json.encodeToJsonElement(data), targetOrigin))
 }
 
+@JvmName("consumeEach")
 suspend fun IframeState.consumeEach(block: (JsonElement, String) -> Unit) {
     for (event in incoming) block(event.data, event.origin)
 }
 
+@JvmName("consumeEachT")
 suspend inline fun <reified T> IframeState.consumeEach(block: (T, String) -> Unit) {
     for (event in incoming) {
         val data = try {
