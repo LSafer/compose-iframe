14665353e04048540b9de73391215b50dfbf28da
lsafer-meemer <135058778+lsafer-meemer@users.noreply.github.com>
2025-09-10 04:16:16 +0300

    feat: convenience messaging utilities

---
+++ compose-iframe/src/commonMain/kotlin/Extensions.kt
@@ -0,0 +1,33 @@
+package net.lsafer.compose.iframe
+
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.decodeFromJsonElement
+import kotlinx.serialization.json.encodeToJsonElement
+
+suspend fun IframeState.emit(data: JsonElement, targetOrigin: String = "*") {
+    outgoing.send(IframeOutgoingEvent(data, targetOrigin))
+}
+
+suspend inline fun <reified T> IframeState.emit(data: T, targetOrigin: String = "*") {
+    outgoing.send(IframeOutgoingEvent(Json.encodeToJsonElement(data), targetOrigin))
+}
+
+suspend fun IframeState.consumeEach(block: (JsonElement, String) -> Unit) {
+    for (event in incoming) block(event.data, event.origin)
+}
+
+suspend inline fun <reified T> IframeState.consumeEach(block: (T, String) -> Unit) {
+    for (event in incoming) {
+        val data = try {
+            Json.decodeFromJsonElement<T>(event.data)
+        } catch (_: SerializationException) {
+            return
+        } catch (_: IllegalArgumentException) {
+            return
+        }
+
+        block(data, event.origin)
+    }
+}
